f2f6482acb17607786981cde6476f65ebf5c4980
syk25 <syk2245@gmail.com>
2024-10-24 03:31:16 +0900

    Rename: html과 js 코드 분리

---
+++ word-relay/index.js
@@ -0,0 +1,29 @@
+const number = Number(prompt("참가자는 몇명인가요?"));
+const $input = document.querySelector("input");
+const $button = document.querySelector("button");
+const $word = document.querySelector("#word");
+const $order = document.querySelector("#order");
+let newWord; // 현재 단어
+let word; // 제시어
+const onInput = (event) => {
+    newWord = event.target.value;
+};
+const onClickButton = () => {
+    if (!word || word.at(-1) === newWord[0]) {
+        // 비어 있음
+        word = newWord;
+        $word.textContent = word;
+        const order = Number($order.textContent);
+        if (order + 1 > number) {
+            $order.textContent = 1;
+        } else {
+            $order.textContent = order + 1;
+        }
+    } else {
+        alert("틀린 단어입니다!");
+    }
+    $input.value = "";
+    $input.focus();
+};
+$input.addEventListener("input", onInput);
+$button.addEventListener("click", onClickButton);
